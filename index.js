38c9b6c15d05608fdfd3f06563eb3c16e4fd0742
Vitali <vitali.altuchow@alumni.fh-aachen.de>
2023-06-19 15:38:12 +0200

    refactor code and update contact links

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
-document.querySelector(".check-button").addEventListener("click", handleClickCheckButton);
 class VocablePair {
-    constructor(de, en) {
-      this.de = de;
-      this.en = en;
-    }
+  constructor(de, en) {
+    this.de = de;
+    this.en = en;
   }
-var germanVocDisplay = document.querySelector(".vocable-text");
-var englishVocInput = document.querySelector(".vocable-input");
+}
+
+function getRandomVocPair(vocables){
+  randomIndex = Math.floor(Math.random() * vocables.length);
+  return vocables[randomIndex];
+}
+
+
 var vocPairs = [new VocablePair("Küche", "kitchen"),
                     new VocablePair("Jacke", "jacket"),
                     new VocablePair("Katze", "cat"),
@@ -17,42 +21,45 @@ var vocPairs = [new VocablePair("Küche", "kitchen"),
                     new VocablePair("Hund", "dog"),
                     new VocablePair("Auto", "car"),
                     new VocablePair("Fahrrad", "bicycle"),
-                    new VocablePair("Schuhe", "shoes"),];
-function getRandomVocPair(vocables){
-                        randomIndex = Math.floor(Math.random() * vocables.length);
-                        return vocables[randomIndex];
-}
+                    new VocablePair("Schuhe", "shoes")];
+
+var germanVocDisplay = document.querySelector(".vocable-german");
+var englishVocInput = document.querySelector(".vocable-english-user-input");
+var checkOrNextButton = document.querySelector(".check-or-next-button");
+
 var currentVocPair = getRandomVocPair(vocPairs);
 germanVocDisplay.textContent = currentVocPair.de;
-var pressedCheck = false;
+checkOrNextButton.textContent = "Check";
 
-function handleClickCheckButton(){
 
-  if(pressedCheck === false){
-    console.log("!" + englishVocInput.value.trim() + "!" + currentVocPair.en);
+function handleClickCheckOrNextButton(){
+
+  if(checkOrNextButton.textContent === "Check"){
+    /* if text of checkOrNextButton was 'Check', check my input and show me the result */
+
     if(englishVocInput.value.trim() == currentVocPair.en){
-      document.querySelector(".vocable-input").classList.add("right-translation");
-      document.querySelector(".vocable-input").value = "Well done!";
+      englishVocInput.classList.add("right-translation");
+      englishVocInput.value = "Well done!";
     }
     else{
-      document.querySelector(".vocable-input").classList.add("wrong-translation");
-      document.querySelector(".vocable-input").value = 'Right Answer: ' + currentVocPair.en;
+      englishVocInput.classList.add("wrong-translation");
+      englishVocInput.value = 'Right Answer: ' + currentVocPair.en;
     }
 
+    englishVocInput.setAttribute("readonly", "readonly");
+    checkOrNextButton.textContent = "Next";
 
-    document.querySelector(".vocable-input").setAttribute("readonly", "readonly");
-    pressedCheck = true;
-    document.querySelector(".check-button").textContent = "Next";
-
-    
   }else{
+    /* else if text of checkOrNextButton was 'Next', give me the next vocable */
+
     currentVocPair = getRandomVocPair(vocPairs);
     germanVocDisplay.textContent = currentVocPair.de;
-    document.querySelector(".check-button").textContent = "Check";
-    document.querySelector(".vocable-input").classList.remove("wrong-translation");
-    document.querySelector(".vocable-input").classList.remove("right-translation");
-    document.querySelector(".vocable-input").value = "";
-    document.querySelector(".vocable-input").removeAttribute("readonly");
-    pressedCheck = false;
+    checkOrNextButton.textContent = "Check";
+    englishVocInput.classList.remove("wrong-translation");
+    englishVocInput.classList.remove("right-translation");
+    englishVocInput.value = "";
+    englishVocInput.removeAttribute("readonly");
   }
 }
+
+checkOrNextButton.addEventListener("click", handleClickCheckOrNextButton);
